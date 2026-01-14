267a12dc2a10ab9ea7f5064782ed3bffb0212893
gd-m <gitanjali.kotwal@gmail.com>
2026-01-14 16:19:11 -0600

    improved rock paper scissor program using list

--- day2_rock_paper_scissors.py
+++ day2_rock_paper_scissors.py
@@ -27,29 +27,19 @@ scissors = '''
 ---.__(___)
 '''
 
-
+choices = [rock, paper, scissors]
 comp_choice = random.randint(0,2)
 
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-
-if user_choice == 0:
-    print(rock)
-elif user_choice == 1:
-    print(paper)
-elif user_choice == 2:
-    print(scissors)
+if user_choice >= 0 and user_choice <=2:
+    print(f"You Chose: \n{choices[user_choice]}")
 else:
     print("Invalid input. You lose.")
 
 if user_choice > 0 and user_choice <= 2:
     print("Computer Chose: ")
 
-    if comp_choice == 0:
-        print(rock)
-    elif user_choice == 1:
-        print(paper)
-    else:
-        print(scissors)
+    print(choices[comp_choice])
 
     if comp_choice == user_choice:
         print("It's a draw")
